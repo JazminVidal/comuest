a210a6a0781be44478c27cae6cc1602e15bd07db
jazminvidal@gmail.com <jazmin.vidald>
2022-10-08 11:41:45 -0300

    changed minor details in readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Comuest
+# Comunicación Estadística
 Repositorio de ejercicios realizados en el taller de Comunicacion Estadistica de la Maestria en Estadistica Matematica de la Universidad de Buenos Aires. 
 
 Contiene: 
@@ -7,4 +7,5 @@ Contiene:
 El ejercicio se encuentra en la carpeta visualizaciones. Contiene los datos, el código y las instrucciones para reproducir un gráfico lollipop y uno de Sankey para visualizar información relativa a la exportación de frutas en Argentina entre los años 2011 y 2017. 
 
 # 2) Ejercicio de Aplicación Shiny
-El ejercicio se encuentra en la carpeta shiny app. Contiene el código para general una aplicación Shiny que permite cargar un ejercicio para entender la relación entre la potenica y el error de tipo 2 de un test de hipótesis para la media de una población normal con varianza conocida y el tamaño de la muestra para distintos niveles de significancia. La aplicación está hosteada en https://inferenciautdt.shinyapps.io/power/.
+El ejercicio se encuentra en la carpeta shiny app. Contiene el código para generar una aplicación Shiny que permite visualizar la relación entre la potenica y el error de tipo 2 de un test de hipótesis para la media de una población normal con varianza conocida y el tamaño de la muestra para distintos niveles de significancia. Un posible uso de la aplicación es compartirla junto con el enunciado de un ejercicio y, luego de resolverlo, verificar los resultados visualmente. 
+La aplicación está hosteada en https://inferenciautdt.shinyapps.io/power/. 
--- shiny_app/power_app.R
+++ shiny_app/power_app.R
@@ -81,14 +81,16 @@ powerg <- function(n = 36, mu0 = 30, mu1 = 31, sd = 6, alpha = 0.10,
   }
   
   
-  p + stat_function(fun = dnorm_fun1, geom = "area", n = 1500, fill = "red", alpha = 0.95) +
-    stat_function(fun = dnorm_fun2, geom = "area", n = 1500, fill = "blue", alpha = 0.4) +
+  p + stat_function(fun = dnorm_fun1, geom = "area", n = 1500, fill = "red", alpha = 0.95, aes(colour = "Hipotetica")) +
+    stat_function(fun = dnorm_fun2, geom = "area", n = 1500, fill = "blue", alpha = 0.4, aes(colour = "Verdadera")) +
     stat_function(fun = dnorm_fun3, geom = "area", n = 1500, fill = "green", alpha = 0.1) +
     stat_function(fun = dnorm, args = list(mu0, sd/sqrt(n)), n = 1500, color = "red") +
     stat_function(fun = dnorm, args = list(mu1, sd/sqrt(n)), n = 1500, color = "blue") +
     geom_hline(yintercept = 0) +
+    scale_colour_manual("Distribución", values = c("red", "blue")) +
     theme_bw(base_size = 16) +
-    labs(x = "", y = "", title = paste0("Potencia ","(",POWER,") es la suma de las áreas azul y rosa"),) +
+    labs(x = "", y = "", title = paste0("Potencia ","(",POWER,") es la suma del area azul y del area roja"),
+         caption = paste0("El error de tipo II ","(",BETA,") es el area verde")) +
     theme(plot.title = element_text(hjust = 0.5)) + 
     theme(plot.caption = element_text(hjust = 0.5))  
 }
@@ -98,16 +100,34 @@ powerg <- function(n = 36, mu0 = 30, mu1 = 31, sd = 6, alpha = 0.10,
 # Primero definimos un Interfaz de usuario (UI)
 ui <- fluidPage(
   withMathJax(),
+  # Estética de los recuadros de texto
+  tags$head(
+    tags$style(HTML("
+            code {
+                display:block;
+                padding:9.5px;
+                margin:0 0 10px;
+                margin-top:10px;
+                font-size:13px;
+                line-height:20px;
+                word-break:break-all;
+                word-wrap:break-word;
+                white-space:pre-wrap;
+                background-color:#F5F5F5;
+                border:1px solid rgba(0,0,0,0.15);
+                border-radius:4px; 
+                font-family:monospace;
+            }"))),
   
   # Titulo
-  titlePanel("Potencia de Test para la Media"),
+  titlePanel("Potencia de Test para la Media de una Población Normal con Varianza Conocida"),
   
   sidebarPanel(
   radioButtons(inputId = "Tipo", label = "Seleccionar hipotesis alternativa:", 
                choices = c("\\(H_1:\\mu \\neq \\mu_0 \\)" = "two.sided",
-                           "\\(H_1:\\mu > \\mu_0 \\)" = "less",
-                           "\\(H_1:\\mu < \\mu_0 \\)" = "greater"),
-               selected = "less"),
+                           "\\(H_1:\\mu > \\mu_0 \\)" = "greater",
+                           "\\(H_1:\\mu < \\mu_0 \\)" = "less"),
+               selected = "two.sided"),
   
   numericInput(inputId = "N", label = "Tamaño de muestra (\\(n\\)):", value = 25, min = 3, max = 10000, step = 1),
   numericInput(inputId = "mu0", label = "Media propuesta \\( \\mu_0 \\):", value = 30, min = -10000, max = 10000, step = 1),
@@ -118,9 +138,14 @@ ui <- fluidPage(
   ),
 # Mostramos un grafico con la potencia
 mainPanel( 
+  code("La función de potencia de un test es la probabilidad de rechazar la
+hipótesis nula cuando es falsa."),
+  code("También se puede ver como la probabilidad de no cometer un error de tipo II."),
+  
   plotOutput("power"), 
-  textInput("caption", "Ejercicio", "Se desea testear..."),
-  verbatimTextOutput("value")
+  # Lineas en caso de querer ingresar texto a mano.
+  #textInput("caption", "Ejercicio", "Se desea testear..."),
+  #verbatimTextOutput("value"), 
   )
 )
 
@@ -136,7 +161,7 @@ server <- function(input, output) {
            mu1 = input$mu1,
            alpha = input$Alpha,
            alternative = input$Tipo)})
-  output$value <- renderText({ input$caption })
+  #output$value <- renderText({ input$caption })
   
 }
 
